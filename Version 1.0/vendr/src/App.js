81e730dbd2e46aa1358a8fcd011256a2286f40ea
Abhik <abhikgupta01@gmail.com>
2024-04-07 20:44:01 +0530

    Version 1.0

---
+++ Version 1.0/vendr/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Homepage from './pages/Homepage';
+
+function App() {
+  return (
+    <div id="main-body">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
