a88ed2e1aab4c1d4e6535bda32196152056c5020
Abhik <abhikgupta01@gmail.com>
2024-04-10 00:51:30 +0530

    Version 4.0

---
+++ Version 4.0/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+
+import Homepage from './pages/Homepage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+// eslint-disable-next-line no-unused-vars
+import { createBrowserRouter, RouterProvider, Route, Link } from "react-router-dom";
+import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/cart",
+    element: <CartPage />,
+  },
+  {
+    path: "/checkout",
+    element: <CheckoutPage />,
+  }
+]);
+
+function App() {
+  return (
+    <div id="main-body">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
