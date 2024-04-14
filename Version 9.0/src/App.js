54a242aa6f4762f80af174f7213fc7cf3cdf1504
Abhik <abhikgupta01@gmail.com>
2024-04-14 22:20:42 +0530

    Version 9.0 - Checkout Page

---
+++ Version 9.0/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './App.css';
+
+import Homepage from './pages/Homepage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+// eslint-disable-next-line no-unused-vars
+import { createBrowserRouter, RouterProvider, Route, Link } from "react-router-dom";
+import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/cart",
+    element: <CartPage />,
+  },
+  {
+    path: "/checkout",
+    element: <CheckoutPage />,
+  },
+  {
+    path: "/product-detail/:id",
+    element: <ProductDetailPage />,
+  }
+]);
+
+function App() {
+  return (
+    <div id="main-body">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
