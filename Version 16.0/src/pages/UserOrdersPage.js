0f72506949dcb128b7c5b03c0c723cd12fe60bf9
Abhik <abhikgupta01@gmail.com>
2024-04-23 03:33:14 +0530

    Version 16.0 - Admin Panel Pages

---
+++ Version 16.0/src/pages/UserOrdersPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../features/navbar/Navbar';
+import UserOrders from '../features/user/components/UserOrders';
+
+function UserOrdersPage() {
+    return (
+        <div>
+            <Navbar>
+                <UserOrders />
+            </Navbar>
+        </div>
+    )
+}
+
+export default UserOrdersPage;
