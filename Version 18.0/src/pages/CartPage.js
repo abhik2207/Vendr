1ab68c4ba37bc8556bc7d6e29c141ba03531713e
Abhik <abhikgupta01@gmail.com>
2024-04-25 04:17:04 +0530

    Version 18.0 - Modal, Toast, Loader

---
+++ Version 18.0/src/pages/CartPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Cart from '../features/cart/Cart';
+
+function CartPage() {
+    return (
+        <div className='w-full h-screen overflow-hidden'>
+            <Cart />
+        </div>
+    )
+}
+
+export default CartPage;
