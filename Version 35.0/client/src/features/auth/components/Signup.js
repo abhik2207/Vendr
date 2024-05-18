0a5204e868454a91af0f4776bc0c92d25a85bef1
Abhik <abhikgupta01@gmail.com>
2024-05-18 21:46:24 +0530

    Final version!!! :)

--- Version 35.0/client/src/features/auth/components/Signup.js
+++ Version 35.0/client/src/features/auth/components/Signup.js
@@ -28,7 +28,7 @@ export default function Signup() {
 
                     <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                         <form noValidate className="space-y-6" action="#" method="POST" onSubmit={handleSubmit((data) => {
-                            dispatch(createUserAsync({ email: data.email, password: data.password, addresses: [], role: 'user' }));
+                            dispatch(createUserAsync({ email: data.email, password: data.password, addresses: [], role: 'user', name: data.name }));
                         })}>
                             <div>
                                 <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
@@ -46,6 +46,23 @@ export default function Signup() {
                                         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     />
                                     {errors.email && <p className='text-red-500 text-xs'>{errors.email.message}</p>}
+                                </div>
+                            </div>
+
+                            <div>
+                                <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">Name</label>
+                                <div className="mt-2">
+                                    <input
+                                        id="name"
+                                        {...register('name',
+                                            {
+                                                required: "Please enter your name"
+                                            }
+                                        )}
+                                        type="name"
+                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                    {errors.name && <p className='text-red-500 text-xs'>{errors.name.message}</p>}
                                 </div>
                             </div>
 
