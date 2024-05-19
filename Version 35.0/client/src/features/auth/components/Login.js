82911a129374dd9d82b662fba05b2ba30a7c7709
Abhik <abhikgupta01@gmail.com>
2024-05-19 16:17:32 +0530

    Version 35.0 - Final Version! :)

--- Version 35.0/client/src/features/auth/components/Login.js
+++ Version 35.0/client/src/features/auth/components/Login.js
@@ -66,7 +66,7 @@ export default function Login() {
                                         type="password"
                                         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     />
-                                    {errors.password && <p className='text-red-500 text-xs'>{errors.email.message}</p>}
+                                    {errors.password && <p className='text-red-500 text-xs'>{errors.password.message}</p>}
                                 </div>
                                 {error && <p className='text-red-500 text-xs'>{error || error.message}</p>}
                             </div>
