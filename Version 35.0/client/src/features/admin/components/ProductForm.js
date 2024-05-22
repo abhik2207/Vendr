80e0ca75ccfe50d9331cbec72326036cd54f65c2
Abhik <abhikgupta01@gmail.com>
2024-05-22 14:45:47 +0530

    Version 35.0 - Final Version! :)

--- Version 35.0/client/src/features/admin/components/ProductForm.js
+++ Version 35.0/client/src/features/admin/components/ProductForm.js
@@ -28,6 +28,9 @@ function ProductForm() {
         { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900', id: 'black' },
         { name: 'Red', class: 'bg-red-500', selectedClass: 'ring-red-900', id: 'red' },
         { name: 'Blue', class: 'bg-blue-500', selectedClass: 'ring-blue-900', id: 'blue' },
+        { name: 'Green', class: 'bg-green-500', selectedClass: 'ring-green-900', id: 'green' },
+        { name: 'Yellow', class: 'bg-yellow-500', selectedClass: 'ring-yellow-900', id: 'yellow' },
+        { name: 'Creme', class: 'bg-amber-100', selectedClass: 'ring-amber-900', id: 'creme' },
         { name: '-', class: 'bg-white', selectedClass: 'ring-gray-400', id: 'std' }
     ];
 
@@ -409,112 +412,6 @@ function ProductForm() {
                                     </div>
                                 </div>
                             </div>
-
-
-
-
-                        </div>
-                    </div>
-
-                    <div className="border-b border-gray-900/10 pb-12">
-                        <h2 className="text-base font-semibold leading-7 text-gray-900">Extras</h2>
-                        <p className="mt-1 text-sm leading-6 text-gray-600">
-                            Extra options here.
-                        </p>
-
-                        <div className="mt-10 space-y-10">
-                            <fieldset>
-                                <legend className="text-sm font-semibold leading-6 text-gray-900">By Email</legend>
-                                <div className="mt-6 space-y-6">
-                                    <div className="relative flex gap-x-3">
-                                        <div className="flex h-6 items-center">
-                                            <input
-                                                id="comments"
-                                                name="comments"
-                                                type="checkbox"
-                                                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                                            />
-                                        </div>
-                                        <div className="text-sm leading-6">
-                                            <label htmlFor="comments" className="font-medium text-gray-900">
-                                                Comments
-                                            </label>
-                                            <p className="text-gray-500">Get notified when someones posts a comment on a posting.</p>
-                                        </div>
-                                    </div>
-                                    <div className="relative flex gap-x-3">
-                                        <div className="flex h-6 items-center">
-                                            <input
-                                                id="candidates"
-                                                name="candidates"
-                                                type="checkbox"
-                                                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                                            />
-                                        </div>
-                                        <div className="text-sm leading-6">
-                                            <label htmlFor="candidates" className="font-medium text-gray-900">
-                                                Candidates
-                                            </label>
-                                            <p className="text-gray-500">Get notified when a candidate applies for a job.</p>
-                                        </div>
-                                    </div>
-                                    <div className="relative flex gap-x-3">
-                                        <div className="flex h-6 items-center">
-                                            <input
-                                                id="offers"
-                                                name="offers"
-                                                type="checkbox"
-                                                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                                            />
-                                        </div>
-                                        <div className="text-sm leading-6">
-                                            <label htmlFor="offers" className="font-medium text-gray-900">
-                                                Offers
-                                            </label>
-                                            <p className="text-gray-500">Get notified when a candidate accepts or rejects an offer.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </fieldset>
-                            <fieldset>
-                                <legend className="text-sm font-semibold leading-6 text-gray-900">Push Notifications</legend>
-                                <p className="mt-1 text-sm leading-6 text-gray-600">These are delivered via SMS to your mobile phone.</p>
-                                <div className="mt-6 space-y-6">
-                                    <div className="flex items-center gap-x-3">
-                                        <input
-                                            id="push-everything"
-                                            name="push-notifications"
-                                            type="radio"
-                                            className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                                        />
-                                        <label htmlFor="push-everything" className="block text-sm font-medium leading-6 text-gray-900">
-                                            Everything
-                                        </label>
-                                    </div>
-                                    <div className="flex items-center gap-x-3">
-                                        <input
-                                            id="push-email"
-                                            name="push-notifications"
-                                            type="radio"
-                                            className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                                        />
-                                        <label htmlFor="push-email" className="block text-sm font-medium leading-6 text-gray-900">
-                                            Same as email
-                                        </label>
-                                    </div>
-                                    <div className="flex items-center gap-x-3">
-                                        <input
-                                            id="push-nothing"
-                                            name="push-notifications"
-                                            type="radio"
-                                            className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                                        />
-                                        <label htmlFor="push-nothing" className="block text-sm font-medium leading-6 text-gray-900">
-                                            No push notifications
-                                        </label>
-                                    </div>
-                                </div>
-                            </fieldset>
                         </div>
                     </div>
                 </div>
